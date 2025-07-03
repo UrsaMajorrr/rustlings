d3f65d8dcc959345d542519925e2ad6726e05126
UrsaMajorrr <carlsonkade3232@gmail.com>
2025-07-03 19:18:05 -0400

    modules and strings

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,18 @@
 fn trim_me(input: &str) -> &str {
-    // TODO: Remove whitespace from both ends of a string.
+    // TODO: Remove whitespace from both ends of a string
+    input.trim()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    let s1 = String::from(input);
+    s1 + " world!"
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    let s1 = String::from(input);
+    s1.replace("cars", "balloons")
 }
 
 fn main() {
